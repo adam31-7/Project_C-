036bded8cf31c0c1536e15d90f9da03c3db959e0
adam31-7 <adam.ladjal@viacesi.fr>
2023-12-06 09:55:29 +0100

    gestiond es stocks (catalogues)

--- Projet-POO/Main.h
+++ Projet-POO/Main.h
@@ -3,6 +3,7 @@
 #include "PageStats.h"
 #include "PageCommande.h"
 #include "PagePersonnel.h"
+#include "PageStock.h"
 
 namespace ProjetPOO {
 
@@ -47,6 +48,7 @@ namespace ProjetPOO {
 	private: System::Windows::Forms::PictureBox^ pictureBox1;
 	private: System::Windows::Forms::PictureBox^ pictureBox2;
 	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Button^ button2;
 
 
 
@@ -75,16 +77,17 @@ namespace ProjetPOO {
 			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
 			this->pictureBox2 = (gcnew System::Windows::Forms::PictureBox());
 			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->button2 = (gcnew System::Windows::Forms::Button());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// bouton_page_clients
 			// 
-			this->bouton_page_clients->Location = System::Drawing::Point(76, 143);
-			this->bouton_page_clients->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
+			this->bouton_page_clients->Location = System::Drawing::Point(68, 114);
+			this->bouton_page_clients->Margin = System::Windows::Forms::Padding(2);
 			this->bouton_page_clients->Name = L"bouton_page_clients";
-			this->bouton_page_clients->Size = System::Drawing::Size(160, 47);
+			this->bouton_page_clients->Size = System::Drawing::Size(142, 38);
 			this->bouton_page_clients->TabIndex = 0;
 			this->bouton_page_clients->Text = L"Gestion Client";
 			this->bouton_page_clients->UseVisualStyleBackColor = true;
@@ -92,21 +95,20 @@ namespace ProjetPOO {
 			// 
 			// bouton_page_statistiques
 			// 
-			this->bouton_page_statistiques->Location = System::Drawing::Point(76, 269);
-			this->bouton_page_statistiques->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
+			this->bouton_page_statistiques->Location = System::Drawing::Point(68, 215);
+			this->bouton_page_statistiques->Margin = System::Windows::Forms::Padding(2);
 			this->bouton_page_statistiques->Name = L"bouton_page_statistiques";
-			this->bouton_page_statistiques->Size = System::Drawing::Size(524, 47);
+			this->bouton_page_statistiques->Size = System::Drawing::Size(223, 38);
 			this->bouton_page_statistiques->TabIndex = 1;
-			this->bouton_page_statistiques->Text = L"Statistiques de l\'entreprise";
+			this->bouton_page_statistiques->Text = L"Statistiques";
 			this->bouton_page_statistiques->UseVisualStyleBackColor = true;
 			this->bouton_page_statistiques->Click += gcnew System::EventHandler(this, &Main::bouton_page_statistiques_Click);
 			// 
 			// bouton_page_commande
 			// 
-			this->bouton_page_commande->Location = System::Drawing::Point(242, 143);
-			this->bouton_page_commande->Margin = System::Windows::Forms::Padding(3, 4, 3, 4);
+			this->bouton_page_commande->Location = System::Drawing::Point(215, 114);
 			this->bouton_page_commande->Name = L"bouton_page_commande";
-			this->bouton_page_commande->Size = System::Drawing::Size(187, 47);
+			this->bouton_page_commande->Size = System::Drawing::Size(166, 38);
 			this->bouton_page_commande->TabIndex = 2;
 			this->bouton_page_commande->Text = L"Gestion Commandes";
 			this->bouton_page_commande->UseVisualStyleBackColor = true;
@@ -114,10 +116,9 @@ namespace ProjetPOO {
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(436, 143);
-			this->button1->Margin = System::Windows::Forms::Padding(3, 4, 3, 4);
+			this->button1->Location = System::Drawing::Point(388, 114);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(164, 47);
+			this->button1->Size = System::Drawing::Size(146, 38);
 			this->button1->TabIndex = 3;
 			this->button1->Text = L"Gestion Personnel";
 			this->button1->UseVisualStyleBackColor = true;
@@ -126,20 +127,18 @@ namespace ProjetPOO {
 			// pictureBox1
 			// 
 			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
-			this->pictureBox1->Location = System::Drawing::Point(76, 92);
-			this->pictureBox1->Margin = System::Windows::Forms::Padding(3, 4, 3, 4);
+			this->pictureBox1->Location = System::Drawing::Point(68, 74);
 			this->pictureBox1->Name = L"pictureBox1";
-			this->pictureBox1->Size = System::Drawing::Size(524, 15);
+			this->pictureBox1->Size = System::Drawing::Size(466, 12);
 			this->pictureBox1->TabIndex = 4;
 			this->pictureBox1->TabStop = false;
 			// 
 			// pictureBox2
 			// 
 			this->pictureBox2->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox2.Image")));
-			this->pictureBox2->Location = System::Drawing::Point(76, 221);
-			this->pictureBox2->Margin = System::Windows::Forms::Padding(3, 4, 3, 4);
+			this->pictureBox2->Location = System::Drawing::Point(68, 177);
 			this->pictureBox2->Name = L"pictureBox2";
-			this->pictureBox2->Size = System::Drawing::Size(524, 15);
+			this->pictureBox2->Size = System::Drawing::Size(466, 12);
 			this->pictureBox2->TabIndex = 5;
 			this->pictureBox2->TabStop = false;
 			// 
@@ -152,18 +151,30 @@ namespace ProjetPOO {
 			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Tai Le", 22.2F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->label1->ForeColor = System::Drawing::SystemColors::ButtonHighlight;
-			this->label1->Location = System::Drawing::Point(210, 27);
+			this->label1->Location = System::Drawing::Point(187, 22);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(244, 57);
+			this->label1->Size = System::Drawing::Size(201, 48);
 			this->label1->TabIndex = 6;
 			this->label1->Text = L"Bienvenue";
 			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(295, 215);
+			this->button2->Margin = System::Windows::Forms::Padding(2);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(239, 38);
+			this->button2->TabIndex = 7;
+			this->button2->Text = L"Gestion des Stocks";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &Main::bouton_page_stock);
+			// 
 			// Main
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(9, 20);
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::SystemColors::ActiveCaption;
-			this->ClientSize = System::Drawing::Size(675, 381);
+			this->ClientSize = System::Drawing::Size(604, 312);
+			this->Controls->Add(this->button2);
 			this->Controls->Add(this->label1);
 			this->Controls->Add(this->pictureBox2);
 			this->Controls->Add(this->pictureBox1);
@@ -171,9 +182,9 @@ namespace ProjetPOO {
 			this->Controls->Add(this->bouton_page_commande);
 			this->Controls->Add(this->bouton_page_statistiques);
 			this->Controls->Add(this->bouton_page_clients);
-			this->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
-			this->MaximumSize = System::Drawing::Size(697, 437);
-			this->MinimumSize = System::Drawing::Size(697, 437);
+			this->Margin = System::Windows::Forms::Padding(2);
+			this->MaximumSize = System::Drawing::Size(622, 359);
+			this->MinimumSize = System::Drawing::Size(622, 359);
 			this->Name = L"Main";
 			this->Text = L"Main";
 			this->Load += gcnew System::EventHandler(this, &Main::Main_Load);
@@ -202,6 +213,11 @@ namespace ProjetPOO {
 		page_personnel.ShowDialog();
 	}
 	private: System::Void Main_Load(System::Object^ sender, System::EventArgs^ e) {
+	}
+
+	private: System::Void bouton_page_stock(System::Object^ sender, System::EventArgs^ e) {
+	ProjetPOO::PageStock page_stock;
+	page_stock.ShowDialog();
 	}
 
 };
---
+++ Projet-POO/PageStock.cpp
@@ -0,0 +1,2 @@
+#include "PageStock.h"
+
